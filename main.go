b87e3f9757ab31ed73b9cb2ab74d9cdbc8322404
tvs <tvansloten@xebia.com>
2018-04-06 14:09:56 +0200

    Changed to a flexible environment

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 )
 
 // init is run before the application starts serving.
-func init() {
+func main() {
 	// Handle all requests with path /hello with the helloHandler function.
 	http.HandleFunc("/", helloHandler)
+
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
