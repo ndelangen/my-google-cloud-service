cc88fdc02e28a28272b61de3b91fe2ee5e77a2bd
tvs <tvansloten@xebia.com>
2018-04-07 14:55:03 +0200

    Added go app back

---
+++ goapp/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/api2/*", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("One of the things Ford Prefect had always found hardest to understand about humans was their habit of continually stating and repeating the very very obvious."))
+	})
+
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
